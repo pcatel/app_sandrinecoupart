0577634f6179b43e7509cc2b9ad9770a06cc8269
pcatel <pcatel@pascalcatel.com>
2023-09-06 08:51:22 +0200

    update

--- lib/ecrans/recettes.dart
+++ lib/ecrans/recettes.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 import '../bottom_navigation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
+import 'login.dart';
 
-//import '../drawer.dart';
-class Recettes extends StatelessWidget {
-  const Recettes({Key? key}) : super(key: key);
+class Recettes extends StatefulWidget {
+  const Recettes({super.key});
+
+  @override
+  RecettesState createState() => RecettesState();
+}
+
+class RecettesState extends State<Recettes> {
+  Future<List<String>> fetchData() async {
+    final response = await http.get(Uri.parse(
+        'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/php/read_recettes.php'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.cast<String>();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     int numberOfContainers = 6;
     double containerHeight =
         MediaQuery.of(context).size.height / numberOfContainers;
+    double textContainerHeight = containerHeight * 1.5;
+
     double containerWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      //drawer: const MyDrawerWidget(),
       appBar: AppBar(
         backgroundColor: const Color(0xFF609a7d),
         title: const Text(
@@ -23,33 +44,114 @@ class Recettes extends StatelessWidget {
               color: Color.fromARGB(255, 1, 1, 1)),
         ),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        height: containerHeight,
-        width: containerWidth,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/recettes.jpg'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Color(0xFF9C27B0),
-                BlendMode.color,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: containerHeight,
+            width: containerWidth,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/recettes.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Color(0xFF9C27B0),
+                  BlendMode.color,
+                ),
               ),
-            )),
-        child: const Text(
-          'Recettes',
-          style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 7, 7, 7)),
-        ),
+            ),
+            child: const Text(
+              'Recettes',
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 7, 7, 7)),
+            ),
+          ),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: SizedBox(
+              height: textContainerHeight,
+              width: containerWidth,
+              child: FutureBuilder<List<String>>(
+                future: fetchData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return const Text('Erreur de chargement des données');
+                  } else if (!snapshot.hasData) {
+                    return const Text('Aucune donnée disponible');
+                  } else {
+                    List<String> dataList = snapshot.data ?? [];
+                    String queryResult = dataList.join('\n');
+                    return SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          queryResult,
+                          style: GoogleFonts.nanumPenScript(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+          Container(
+            height: containerHeight,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: 200, // Définissez la largeur souhaitée pour les boutons
+                  height: 50,  // Définissez la hauteur souhaitée pour les boutons
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Logique de navigation pour "Voir les recettes" à implémenter ici
+                      Navigator.pushNamed(context, '/voirRecettes');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF9C27B0), // Couleur du bouton
+                    ),
+                    child: const Text('Voir les recettes'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16, // Espace vertical entre les boutons
+                ),
+                SizedBox(
+                  width: 200, // Définissez la largeur souhaitée pour les boutons
+                  height: 50,  // Définissez la hauteur souhaitée pour les boutons
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Logique de navigation pour "Me connecter" à implémenter ici
+                      Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Login()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF9C27B0), // Couleur du bouton
+                    ),
+                    child: const Text('Me connecter'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: const BottomNavigationBarScreen(
-        backgroundColor: Color(0xFF9C27B0),
-      ),
+      bottomNavigationBar:
+          const BottomNavigationBarScreen(backgroundColor: Color(0xFF9C27B0)),
     );
   }
 }
-//test
