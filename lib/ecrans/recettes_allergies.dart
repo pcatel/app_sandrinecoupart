e6a16008d4d1306b3a824ca9051baa15061b5efd
pcatel <pcatel@pascalcatel.com>
2023-09-07 16:45:47 +0200

    recettes par allergies

--- lib/ecrans/recettes_allergies.dart
+++ lib/ecrans/recettes_allergies.dart
@@ -4,16 +4,15 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class RecettesAllergies extends StatefulWidget {
-  const RecettesAllergies({super.key});
+  const RecettesAllergies({Key? key}) : super(key: key);
 
   @override
   RecettesAllergiesState createState() => RecettesAllergiesState();
 }
 
 class RecettesAllergiesState extends State<RecettesAllergies> {
-  Future<List<String>> fetchData() async {
+  Future<List<String>> fetchMutuelleData() async {
     final response = await http.get(Uri.parse(
         'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/php/read_recettes_allergies.php'));
 
@@ -25,23 +24,35 @@ class RecettesAllergiesState extends State<RecettesAllergies> {
     }
   }
 
+  Future<List<Map<String, String>>> fetchCommentairesData() async {
+    final response = await http.get(Uri.parse(
+        'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/php/read_allergies.php'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body);
+      List<Map<String, String>> data = [];
+      for (var item in jsonData) {
+        data.add(
+            {"nom": item['nom'], "Id": item['Id']});
+      }
+      return data;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    int numberOfContainers = 6;
-    double containerHeight =
-        MediaQuery.of(context).size.height / numberOfContainers;
-    double textContainerHeight = containerHeight * 1.5;
-
-    double containerWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF609a7d),
         title: const Text(
           'Sandrine Coupart : Diététicienne - Nutritionniste',
           style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 1, 1, 1)),
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 1, 1, 1),
+          ),
         ),
       ),
       body: Column(
@@ -49,13 +60,14 @@ class RecettesAllergiesState extends State<RecettesAllergies> {
         children: [
           Container(
             alignment: Alignment.center,
-            height: containerHeight,
-            width: containerWidth,
+            height: 200,
+            width: MediaQuery.of(context).size.width,
             decoration: const BoxDecoration(
               color: Colors.white,
               image: DecorationImage(
                 image: NetworkImage(
-                    'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/recettes.jpg'),
+                  'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/consultation.jpg',
+                ),
                 fit: BoxFit.cover,
                 colorFilter: ColorFilter.mode(
                   Color(0xFF9C27B0),
@@ -64,20 +76,43 @@ class RecettesAllergiesState extends State<RecettesAllergies> {
               ),
             ),
             child: const Text(
-              'RecettesAllergies',
+              'Recettes par allergies',
               style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 253, 253, 253)),
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 7, 7, 7),
+              ),
             ),
           ),
-          FittedBox(
-            fit: BoxFit.scaleDown,
-            child: SizedBox(
-              height: textContainerHeight,
-              width: containerWidth,
-              child: FutureBuilder<List<String>>(
-                future: fetchData(),
+          FutureBuilder<List<String>>(
+            future: fetchMutuelleData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return const Text('Erreur de chargement des données');
+              } else if (!snapshot.hasData) {
+                return const Text('Aucune donnée disponible');
+              } else {
+                List<String> dataList = snapshot.data ?? [];
+                String queryResult = dataList.join('\n');
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    queryResult,
+                    style: GoogleFonts.nanumPenScript(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                );
+              }
+            },
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: FutureBuilder<List<Map<String, String>>>(
+                future: fetchCommentairesData(),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const CircularProgressIndicator();
@@ -86,15 +121,34 @@ class RecettesAllergiesState extends State<RecettesAllergies> {
                   } else if (!snapshot.hasData) {
                     return const Text('Aucune donnée disponible');
                   } else {
-                    List<String> dataList = snapshot.data ?? [];
-                    String queryResult = dataList.join('\n');
+                    List<Map<String, String>> dataList = snapshot.data ?? [];
                     return SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          queryResult,
-                          style: GoogleFonts.nanumPenScript(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+                      scrollDirection: Axis.horizontal,
+                      child: SingleChildScrollView(
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: DataTable(
+                            headingRowColor: MaterialStateColor.resolveWith(
+                                (states) => const Color(0xFF9C27B0)),
+                            columns: const [
+                              DataColumn(label: Text('nom')),
+                              DataColumn(label: Text('Id')),
+                            ],
+                            rows: dataList.map((data) {
+                              return DataRow(cells: [
+                                DataCell(SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.2,
+                                  child: Text(data['nom'] ?? ''),
+                                )),
+                                DataCell(SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.7,
+                                  child: Text(data['Id'] ?? ''),
+                                )),
+                              ]);
+                            }).toList(),
+                          ),
                         ),
                       ),
                     );
@@ -103,7 +157,6 @@ class RecettesAllergiesState extends State<RecettesAllergies> {
               ),
             ),
           ),
-     
         ],
       ),
       bottomNavigationBar:
---
+++ lib/ecrans/recettes_allergies_original.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import '../bottom_navigation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class RecettesAllergies extends StatefulWidget {
+  const RecettesAllergies({super.key});
+
+  @override
+  RecettesAllergiesState createState() => RecettesAllergiesState();
+}
+
+class RecettesAllergiesState extends State<RecettesAllergies> {
+  Future<List<String>> fetchData() async {
+    final response = await http.get(Uri.parse(
+        'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/php/read_recettes_allergies.php'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.cast<String>();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int numberOfContainers = 6;
+    double containerHeight =
+        MediaQuery.of(context).size.height / numberOfContainers;
+    double textContainerHeight = containerHeight * 1.5;
+
+    double containerWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF609a7d),
+        title: const Text(
+          'Sandrine Coupart : Diététicienne - Nutritionniste',
+          style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 1, 1, 1)),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: containerHeight,
+            width: containerWidth,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://pascalcatel.com/maquettes/sandrineCoupart/appmobile/recettes.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Color(0xFF9C27B0),
+                  BlendMode.color,
+                ),
+              ),
+            ),
+            child: const Text(
+              'RecettesAllergies',
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 253, 253, 253)),
+            ),
+          ),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: SizedBox(
+              height: textContainerHeight,
+              width: containerWidth,
+              child: FutureBuilder<List<String>>(
+                future: fetchData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return const Text('Erreur de chargement des données');
+                  } else if (!snapshot.hasData) {
+                    return const Text('Aucune donnée disponible');
+                  } else {
+                    List<String> dataList = snapshot.data ?? [];
+                    String queryResult = dataList.join('\n');
+                    return SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          queryResult,
+                          style: GoogleFonts.nanumPenScript(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+     
+        ],
+      ),
+      bottomNavigationBar:
+          const BottomNavigationBarScreen(backgroundColor: Color(0xFF9C27B0)),
+    );
+  }
+}
